dd2ef80962052aeeb85e9ac538eaaefeaa7b8d79
ryusei <perusonaryu1127@gmail.com>
2022-07-24 15:34:17 +0900

    create get news data

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,16 +3,39 @@ import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.scss';
 import MainLayout from '../src/layouts';
+import { GetStaticProps } from 'next';
+import Article from '../src/components/article';
 
-const Home: NextPage = () => {
+function Home(props:any) {
+  // 記事を取得できているか確認
   return (
     <MainLayout>
       <Head>
         <title>Simple News</title>
       </Head>
-      
+      {/* Articleコンポーネントを追加 */}
+      <div className={styles.main}>
+        <Article title="headlines" articles={props.topArticles} />
+      </div>
     </MainLayout>
   );
+}
+
+export const getStaticProps: GetStaticProps = async() => {
+  // NewsAPIのトップ記事の情報を取得
+  const pageSize= 10;
+  const topRes= await fetch(
+    `${process.env.NEXT_PUBLIC_NEWS_URL}?country=jp&pageSize=${pageSize}&apiKey=${process.env.NEXT_PUBLIC_NEWS_API}`
+  );
+
+  const res = await  topRes.json();
+  const topArticles = res?.articles;
+  return {
+    props: {
+      topArticles,
+    },
+    revalidate: 60 * 10,
+  };
 };
 
 export default Home;
---
+++ src/components/article/index.tsx
@@ -0,0 +1,47 @@
+import styles from './index.module.scss';
+import moment from 'moment';
+import Props from '../types';
+import Image from 'next/image';
+
+const Article: React.FC<Props> = ({ articles, title }) => {
+  return (
+    <section className={styles.article}>
+      <div className={styles.article__heading}>
+        {title && (
+          <h1>
+            {title.charAt(0).toUpperCase() + title.slice(1).toLowerCase()}
+          </h1>
+        )}
+      </div>
+      {articles &&
+        articles.map((article, index) => {
+          const time = moment(article.publishedAt || moment.now())
+            .fromNow()
+            .slice(0, 1);
+          return (
+            <a href={article.url} key={index} target="_blank" rel="noreferrer">
+              <article className={styles.article__main}>
+                <div className={styles.article__title}>
+                  <p>{article.title}</p>
+                  <p className={styles.article__time}>
+                    {time}
+                    時間前
+                  </p>
+                </div>
+                {article.urlToImage && (
+                  <img
+                    key={index}
+                    src={article.urlToImage}
+                    className={styles.article__img}
+                    alt={`${article.title} image`}
+                  />
+                )}
+              </article>
+            </a>
+          );
+        })}
+    </section>
+  );
+};
+
+export default Article;
